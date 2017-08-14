0d09523483a8533a3acc1e6e5d2ae8a853ff15f7
刘建 <liujian@digisky.com>
2017-08-14 16:10:15 +0800

    5

--- src/com/tools/jdbc/proxy/JdbcOperateManagerProxy.java
+++ src/com/tools/jdbc/proxy/JdbcOperateManagerProxy.java
@@ -1,47 +1,23 @@
 package com.tools.jdbc.proxy;
 
-import java.io.File;
-
 import javax.sql.DataSource;
 
 import com.tools.jdbc.JdbcOperate;
-import com.tools.jdbc.c3p0.C3P0PropertiesConfig;
-import com.tools.jdbc.factory.JdbcOperateManagerReadWrite;
 
 public class JdbcOperateManagerProxy {
 	
-	public static final String READ = "read";
-	public static final String WRITE = "write";
-	private static JdbcOperateManagerReadWrite manager = null;
-	public static String readPath = "";
-	public static String writePath = "";
+	private static DataSource dataSource = null;
+	private static JdbcOperate jdbcOperate = null;
 	
 	
-	public static void init(){
-		if(manager == null){
-			manager = JdbcOperateManagerReadWrite.newManager(readPath, writePath);
-		}
+	public static JdbcOperate getJdbcOperate() {
+		return jdbcOperate;
 	}
 	
-	public static JdbcOperate getJdbcOperate(String type) {
-		init();
-		if(READ.equals(type)){
-			return manager.getReadJdbcOperate();
-		}else if(WRITE.equals(type)){
-			return manager.getWriteJdbcOperate();
-		}
-		return null;
+	public static DataSource getDataSource() {
+		return dataSource;
 	}
 	
-	public static DataSource getDataSource(String type) {
-		init();
-		if(READ.equals(type)){
-			return manager.getReadDataSource();
-		}else if(WRITE.equals(type)){
-			return manager.getWriteDataSource();
-		}
-		return null;
-	}
 	
 
 }
